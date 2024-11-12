6b9e2d22862edf91aab1866f8bfc029a2f7e5dc9
Kiran0107--Krypto <kbs.jnv@gmail.com>
2024-11-12 18:57:59 +0530

    Kiran- 9 Nov 2024 -Commit and Push -Tasks

---
+++ Nov/ex_09112014_Literals_Variables/Lab029_Task1.py
@@ -0,0 +1,24 @@
+#Task for 9th Nov
+#Take 3 input from User
+#Perform Sum, Subtraction, Multiplication and Division
+
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+num3 = int(input("Enter third number"))
+
+#Sum of three numbers
+Sum = num1+num2+num3
+print(Sum)
+
+#Subtraction
+Sub=num1 - num2 - num3
+print(Sub)
+
+#Multiplication
+Mul= num1*num2*num3
+print(Mul)
+
+#Division
+Div=num1/num2/num3
+print(Div)
+
---
+++ Nov/ex_09112014_Literals_Variables/Lab30_Task2.py
@@ -0,0 +1,21 @@
+#Nov 9th Task 2
+#Take 2 input from the user
+#Print the Quotient and Remainder
+#15 -> num1
+#2 ->num2
+
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+#First method
+
+Quotient1= num1//num2
+print("Quotient is: ",Quotient1)
+
+Remainder1=num1%num2
+print("Remainder is: ",Remainder1)
+
+
+
+
+
