bb440b990f01fba21d7299de4705659619d42604
Kiran0107--Krypto <kbs.jnv@gmail.com>
2024-11-26 00:32:40 +0530

    Kiran - 23 Nov

--- Nov/ex_23112024_Lambda_List_Tuple_SET/Lab105_SET_IQ.py
+++ Nov/ex_23112024_Lambda_List_Tuple_SET/Lab105_SET_IQ.py
@@ -7,7 +7,20 @@ def first_non_repeating(string):
             return char
     return None
 
+#Print all non-repeating charaters
+def all_non_repeating(string):
+    for char in string:
+        if string.count(char)==1:
+            print(char,end=" ")
+    return None
+
 print(first_non_repeating('swiss'))
 print(first_non_repeating('pepper'))
 print(first_non_repeating('dada'))
 print(first_non_repeating('annusinha'))
+
+print("=======================")
+print(all_non_repeating('swiss'))
+print(all_non_repeating('pepper'))
+print(all_non_repeating('dada'))
+print(all_non_repeating('annusinha'))
